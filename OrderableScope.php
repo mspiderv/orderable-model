a0da893be9e3908f8e3891d8c2c6d469998c6f45
mspiderv <mspiderv@gmail.com>
2015-07-31 22:57:13 +0200

    Typo fixed

--- OrderableScope.php
+++ OrderableScope.php
@@ -8,41 +8,41 @@ use Illuminate\Database\Eloquent\Model;
 
 class OrderableScope implements ScopeInterface
 {
-	/**
-	 * Apply the scope to a given Eloquent query builder.
-	 *
-	 * @param  \Illuminate\Database\Eloquent\Builder  $builder
-	 * @return void
-	 */
-	public function apply(Builder $builder, Model $model)
-	{
-		$builder->getQuery()->orderBy($model->getOrderColumnName(), $model->getOrderColumnDirection());
-	}
+    /**
+    * Apply the scope to a given Eloquent query builder.
+    *
+    * @param  \Illuminate\Database\Eloquent\Builder  $builder
+    * @return void
+    */
+    public function apply(Builder $builder, Model $model)
+    {
+        $builder->getQuery()->orderBy($model->getOrderColumnName(), $model->getOrderColumnDirection());
+    }
 
-	/**
-	 * Remove the scope from the given Eloquent query builder.
-	 *
-	 * @param  \Illuminate\Database\Eloquent\Builder  $builder
-	 * @return void
-	 */
-	public function remove(Builder $builder, Model $model)
-	{
-		$query = $builder->getQuery();
-		$property = $query->unions ? 'unionOrders' : 'orders';
-		$orderColumn = $model->getOrderColumnName();
-		$orderDirection = $model->getOrderColumnDirection();
+    /**
+    * Remove the scope from the given Eloquent query builder.
+    *
+    * @param  \Illuminate\Database\Eloquent\Builder  $builder
+    * @return void
+    */
+    public function remove(Builder $builder, Model $model)
+    {
+        $query = $builder->getQuery();
+        $property = $query->unions ? 'unionOrders' : 'orders';
+        $orderColumn = $model->getOrderColumnName();
+        $orderDirection = $model->getOrderColumnDirection();
 
         foreach ($query->{$property} as $key => $order)
         {
-        	if ($order['column'] == $orderColumn && $order['direction'] == $orderDirection)
-        	{
-        		unset($query->{$property}[$key]);
-        	}
+            if ($order['column'] == $orderColumn && $order['direction'] == $orderDirection)
+            {
+                unset($query->{$property}[$key]);
+            }
         }
 
         if (is_array($query->{$property}) && count($query->{$property}) == 0)
         {
-        	$query->{$property} = null;
+            $query->{$property} = null;
         }
-	}
+    }
 }
