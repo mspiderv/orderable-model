edc25b87ef8b23e25dd4fa956beb5fecd283179c
mspiderv <mspiderv@gmail.com>
2015-08-05 01:49:55 +0200

    Added lang

---
+++ Resources/lang/sk/main.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'orderSuccess' => 'Nové poradie bolo uložené.',
+    'orderSuccessDescription' => '',
+
+    'orderError' => 'Položky sa nepodarilo zoradiť.',
+	'orderErrorDescription' => 'Podrobnosti nájdete v konzole.',
+
+];
